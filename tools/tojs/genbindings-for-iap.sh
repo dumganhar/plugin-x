0ccf3e95e39a9b4867fafd9ac0d9f62949f9579b
James Chen <jianhua.chen@cocos2d-x.org>
2013-03-05 16:02:18 +0800

    Adding bash scripts for different modules.

---
+++ tools/tojs/genbindings-for-iap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./genbindings-protocols.sh
+./genbindings-alipay.sh
+./genbindings-chinamobile.sh
+./genbindings-chinatelecom.sh
+./genbindings-chinaunicom.sh
+./genbindings-nd91.sh
+./genbindings-punchbox.sh
